d966afe1cd04ae96c7db66b4bf83aecc61512a57
ivanlopezmolina <ivanlopezmolina@gmail.com>
2017-06-06 17:41:27 -0400

    Models
    models creation

---
+++ FirstASPNetCore/FirstASPNetCore/Models/IPieRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace FirstASPNetCore.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> Pies { get; }
+        IEnumerable<Pie> PiesOfWeek { get; }
+
+        Pie GetPieById(int pieId);
+    }
+}
--- FirstASPNetCore/FirstASPNetCore/Startup.cs
+++ FirstASPNetCore/FirstASPNetCore/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FirstASPNetCore.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,8 @@ namespace FirstASPNetCore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
+            services.AddTransient<IPieRepository, MockPieRepository>();
             services.AddMvc();
         }
 
